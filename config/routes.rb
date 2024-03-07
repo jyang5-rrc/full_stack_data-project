43362d24dc44021c773d89de10f095faa5540ae0
jyang5 <jyang5@academic.rrc.ca>
2024-03-06 21:36:24 -0600

    category controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home', to: 'home#index'
   get 'about', to: 'static_pages#about'
+  get 'category', to: 'categories#category', as: 'category'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
